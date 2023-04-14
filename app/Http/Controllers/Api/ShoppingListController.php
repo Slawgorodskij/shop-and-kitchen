16288686114d4ecedce87129225120ff0fa75a43
vladimir <slawgorodskij@mail.ru>
2023-04-14 21:47:01 +0700

    реализовано добавление списка покупок в БД

--- app/Http/Controllers/Api/ShoppingListController.php
+++ app/Http/Controllers/Api/ShoppingListController.php
@@ -18,21 +18,34 @@ class ShoppingListController extends Controller
         $structures = Structure::where('recipe_id', $data['recipes_id'])->get();
 
         foreach ($structures as $structure) {
+
+            $shoppingList = ShoppingList::where('users_id', $data['users_id'])
+                ->where('product_id', $structure->product_id)
+                ->get();
             $storeroom = Storeroom::where('users_id', $data['users_id'])
                 ->where('product_id', $structure->product_id)
                 ->get();
 
-            if (count($storeroom) === 0) {
-                ShoppingList::create([
-                    'users_id' => $data['users_id'],
-                    'product_id' => $structure->product_id,
-                    'units_id' => $structure->units_id,
-                    'quantity' => $structure->quantity,
-                ]);
-            }
+            if (isset($shoppingList) && count($shoppingList) > 0) {
+                $shoppingList[0]->quantity += $structure->quantity;
+                $shoppingList[0]->save();
+            } elseif (isset($storeroom)
+                && count($storeroom) > 0) {
+                foreach ($storeroom as $item) {
+                    if ($item->quantity > $structure->quantity) {
+                        $item->quantity -= $structure->quantity;
+                        $item->save();
+                    }
+                }
+            } else {
+                $newQuantity = $structure->quantity;
 
-            if (count($storeroom) > 0 && $storeroom->quantity < $structure->quantity) {
-                $newQuantity = $structure->quantity - $storeroom->quantity;
+                if (isset($storeroom)
+                    && count($storeroom) > 0
+                    && $storeroom[0]->quantity < $newQuantity) {
+                    $newQuantity -= $storeroom[0]->quantity;
+                }
+
                 ShoppingList::create([
                     'users_id' => $data['users_id'],
                     'product_id' => $structure->product_id,
@@ -40,7 +53,6 @@ class ShoppingListController extends Controller
                     'quantity' => $newQuantity,
                 ]);
             }
-
         }
         $shoppingList = ShoppingList::all();
 
